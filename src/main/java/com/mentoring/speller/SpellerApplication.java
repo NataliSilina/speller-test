789f8e0c7a80fa7ff945ef6501463e154efeb025
Nataliia Gruzdeva <Nataliia.Silina@thomsonreuters.com>
2018-11-04 18:53:06 +0300

    Init

---
+++ src/main/java/com/mentoring/speller/SpellerApplication.java
@@ -0,0 +1,12 @@
+package com.mentoring.speller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mentoring/speller/SpellerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mentoring.speller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpellerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
